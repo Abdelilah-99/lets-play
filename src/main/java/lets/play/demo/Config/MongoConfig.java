886eb827b9ce790b729b26a2923f8ab60cb3dcca
babdelil <bouchikhiabdelilah0@gmail.com>
2026-01-10 11:08:10 +0100

    mongo conf

---
+++ src/main/java/lets/play/demo/Config/MongoConfig.java
@@ -0,0 +1,50 @@
+package lets.play.demo.Config;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+
+@Configuration
+public class MongoConfig extends AbstractMongoClientConfiguration {
+
+    @Value("${spring.data.mongodb.username}")
+    private String username;
+
+    @Value("${spring.data.mongodb.password}")
+    private String password;
+
+    @Value("${spring.data.mongodb.host}")
+    private String host;
+
+    @Value("${spring.data.mongodb.port}")
+    private int port;
+
+    @Value("${spring.data.mongodb.database}")
+    private String database;
+
+    @Value("${spring.data.mongodb.authentication-database}")
+    private String authenticationDatabase;
+
+    @Override
+    protected String getDatabaseName() {
+        return database;
+    }
+
+    @Override
+    @Bean
+    public MongoClient mongoClient() {
+        String connectionString = String.format(
+            "mongodb://%s:%s@%s:%d/%s?authSource=%s",
+            username,
+            password,
+            host,
+            port,
+            database,
+            authenticationDatabase
+        );
+        return MongoClients.create(connectionString);
+    }
+}
--- src/main/java/lets/play/demo/DTOs/RegisterReqDto.java
+++ src/main/java/lets/play/demo/DTOs/RegisterReqDto.java
@@ -1,2 +1,2 @@
 package lets.play.demo.DTOs;
-public record RegisterReqDto(String name, String password, String email, String role, String id) {}
+public record RegisterReqDto(String name, String password, String email, String role) {}
