d40ddccaab88e9982033f25f9c99bfa41475a716
babdelil <bouchikhiabdelilah0@gmail.com>
2026-01-17 23:42:28 +0100

    ad debug part

--- src/main/java/lets/play/demo/LetsPlayApplication.java
+++ src/main/java/lets/play/demo/LetsPlayApplication.java
@@ -2,7 +2,6 @@ package lets.play.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
 
 @SpringBootApplication()
 public class LetsPlayApplication {
